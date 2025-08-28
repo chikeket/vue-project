2cfa825f67306685b133843725008dee1662ea9d
chikeket <minyounki@gmail.com>
2025-08-28 17:51:45 +0900

    vue추가

---
+++ src/views/DataBindingAttribute.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <img v-bind:src="imgSrc" v-bind:width="widthVal" />
+  </div>
+  <div class="container" v-bind:class="{ active: isActive, 'text-red': isRed }">
+    Class Binding
+  </div>
+  <div v-bind:class="clsAry">Class Binding 배열</div>
+  <!--:태그 이렇게 적어도 v-bind 적용됨-->
+  <div :style="backVal">Style Binding</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      imgSrc: require("@/assets/logo.png"),
+      widthVal: "100px",
+      isActive: false,
+      isRed: true,
+      activeCls: "active", //따로 써서 v-bind:class=""에 넣어도 된다
+      redCls: "text-red",
+      clsAry: ["active", "text-red"], //같이 써도 되고
+      backVal: { backgroundColor: "red", color: "green" },
+    };
+  },
+};
+</script>
+<!-- scoped 이 페이지 내에서만 적용 되는 기능임 -->
+<style scoped>
+.active {
+  background-color: green;
+  font-weight: bold;
+  color: white;
+}
+.text-red {
+  color: red;
+}
+</style>
---
+++ src/views/DataBindingList.vue
@@ -0,0 +1,62 @@
+<template>
+  <h3>{{ msg }}</h3>
+  <table>
+    <thead>
+      <tr>
+        <th>아이디</th>
+        <th>이름</th>
+        <th>나이</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-bind:key="i" v-for="(prod, i) in data">
+        <td>{{ prod.id }}</td>
+        <td v-text="prod.name"></td>
+        <td>{{ prod.age }}</td>
+      </tr>
+    </tbody>
+  </table>
+  <table>
+    <thead>
+      <tr>
+        <th>카테고리</th>
+        <th>배송비</th>
+        <th>가격</th>
+        <th>상품</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-bind:key="i" v-for="(prod, i) in prodList">
+        <td>{{ prod.category }}</td>
+        <td v-text="prod.delivery_price"></td>
+        <td>{{ prod.price }}</td>
+        <td>{{ prod.product_name }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      msg: "학생목록",
+      data: [
+        { id: 1, name: "홍길동", age: 20 },
+        { id: 2, name: "김민수", age: 22 },
+        { id: 3, name: "최익수", age: 23 },
+      ],
+      prodList: [],
+    };
+  },
+  mounted() {
+    // DataBindingList 컴포넌트가 생성이 되면
+    fetch("http://192.168.0.83:3000/products")
+      .then((resopnse) => resopnse.json())
+      .then((result) => {
+        console.log(result);
+        this.prodList = result;
+      });
+  },
+};
+</script>
+<style></style>
